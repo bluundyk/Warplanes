49c8151a3066f541e2cc827ef2e78ee6a917d56c
bluundyk <fromenlesan2@gmail.com>
2026-02-07 15:54:15 +0300

    a prototype of the menu has been created

---
+++ MenuFolder/menu.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://Game/game.tscn")
+
+
+func _on_settings_pressed() -> void:
+	get_tree().change_scene_to_file("res://Settings/settings.tscn")
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
