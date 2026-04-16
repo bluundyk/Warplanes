f3616369225504142c0671585395a7f63705979b
Tashlintsev <tashlintsev-d-a@yandex.ru>
2026-04-16 15:49:38 +0400

    Исправил счётчик монет в меню

--- UpgradeSelectionMenu/SelectionUPGRD.gd
+++ UpgradeSelectionMenu/SelectionUPGRD.gd
@@ -1,5 +1,23 @@
 extends Node2D
 
+@onready var total_coins_label = $TotalCoins
+
+const TOTAL_COINS_SAVE_PATH = "user://total_coins.save"
+
+func _ready():
+	update_total_coins()
+
+func update_total_coins():
+	var total_coins = 0
+
+	if FileAccess.file_exists(TOTAL_COINS_SAVE_PATH):
+		var file = FileAccess.open(TOTAL_COINS_SAVE_PATH, FileAccess.READ)
+		if file:
+			total_coins = int(file.get_as_text())
+
+	if total_coins_label:
+		total_coins_label.text = str(total_coins)
+
 func _on_levels_pressed() -> void:
 	get_tree().change_scene_to_file("res://LevelSelestionMenu/LevelSelection.tscn")
 
